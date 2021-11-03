f7d35d46cdafd8460c8f58e4df6adf2af4b8aa74
Arijit Roy <roy.arijit2001@gmail.com>
2021-11-03 21:25:26 +0530

    ⚡️ stable: `plagiarism checker`

---
+++ src/scripts/assignment.py
@@ -0,0 +1,96 @@
+import file_handler
+from scripts.file_handler import DUMP_DIR
+
+import os
+import re
+import docx
+import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.metrics.pairwise import cosine_similarity
+
+def cache_assignments(course_code: str, assignment_id: str):
+    """Downloads all the answers to an assignment to DUMP
+
+    Args:
+        course_code (str): course code of the assignment
+        assignment_id (str): assignment id 
+    """
+
+    files_list = file_handler.list_files("obsidian")
+    BASE_STR = f"{course_code}/assignment/{assignment_id}/answers/"
+    answers_list = [file.key for file in files_list if file.key.startswith(BASE_STR)]
+    
+    for file in answers_list:
+        if file_handler.download_files("obsidian", file):
+            print(f"[LOG] Downloaded {file.split('/')[-1]}")
+        
+
+def word_to_str(path: str):
+    doc = docx.Document(path)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+
+    return ' '.join(fullText)
+
+
+
+def plag_util(doc_id, files_list):
+    #* read all files with .docx
+    #* create a list of dict
+    #* create pd.DataFrame
+    file_content = []
+    for file in files_list:
+        temp_dict = {
+            "content": word_to_str(os.path.join(DUMP_DIR, file)),
+            "name": file.split('.')[0]
+        }
+        file_content.append(temp_dict)
+
+    documents_df = pd.DataFrame(file_content)
+    stop_words_english=stopwords.words('english')
+    documents_df['content'] = documents_df["content"].apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]',' ',w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]',' ',w).lower() not in stop_words_english))
+
+
+    tfidf = TfidfVectorizer(max_features = 64)
+    tfidf.fit(documents_df["content"])
+
+    vectors = tfidf.transform(documents_df["content"])
+
+    similarity_matrix = cosine_similarity(vectors, vectors)
+    similar_ix=np.argsort(similarity_matrix[doc_id])[::-1]
+
+    result = []
+    
+    for ix in similar_ix:
+        if ix==doc_id:
+            continue
+        # print(documents_df.iloc[ix]["name"], similarity_matrix[doc_id][ix])
+        name_sim_dict = {
+        "name": documents_df.iloc[ix]['name'],
+        "sim_score": str(similarity_matrix[doc_id][ix]*100)
+        }
+        result.append(name_sim_dict)
+    return result
+
+
+def check_plagiarism():
+    files_list = os.listdir(DUMP_DIR)
+    files_list = [f for f in  files_list if f[-4:] == 'docx']
+
+    results = []
+
+    for ctr, file in enumerate(files_list):
+        temp_dict = {
+            "name": file.split('.')[0],
+            "result": plag_util(ctr, files_list)
+        }
+        results.append(temp_dict)
+    
+    return results
+
+if __name__ == "__main__":
+    print(check_plagiarism())
+    
